2823f2bddd160b2b31e2d670925b31902b37c469
pradikaaaaa <pradikanizer13@gmail.com>
2020-11-30 14:45:59 +0700

    update mailer.php

--- application/libraries/Mailer.php
+++ application/libraries/Mailer.php
@@ -14,11 +14,11 @@ class Mailer
 
     protected $_ci;
 
-    protected $email_pengirim = 'sinarmudakreatif@ikionline.id'; // Isikan dengan email pengirim
+    protected $email_pengirim = 'ikionline@ikionline.id'; // Isikan dengan email pengirim
 
     protected $nama_pengirim = 'Sinar Muda Kreatif'; // Isikan dengan nama pengirim
 
-    protected $password = '1000Ribu'; // Isikan dengan password email pengirim
+    protected $password = 'ikionlinesmk123'; // Isikan dengan password email pengirim
 
 
 
